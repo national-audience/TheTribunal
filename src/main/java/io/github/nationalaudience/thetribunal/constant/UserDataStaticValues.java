48ed49ea175bb8b3af842f500d45a71da3a511c9
jvicog <javiervico8@gmail.com>
2022-02-15 14:37:40 +0100

    UserData

---
+++ src/main/java/io/github/nationalaudience/thetribunal/constant/UserDataStaticValues.java
@@ -0,0 +1,14 @@
+package io.github.nationalaudience.thetribunal.constant;
+
+public class UserDataStaticValues {
+
+    public static final String END_POINT_USER_DATA = "/user/{inUsername}";
+
+    public static final String PARAMETER_USER = "inUsername";
+
+    public static final String ATTRIBUTE_USER = "user";
+
+    public static final String TEMPLATE_USER_DATA = "data/userdata_template";
+    public static final String TEMPLATE_USER_DATA_NOT_FOUND = "data/userdata_not_found_template";
+
+}
