9538b01a38c22dab0ca5d1845dc82e5e17490741
Gael Rial Costas <gael.rial.costas@gmail.com>
2022-02-17 11:20:08 +0100

    Add signup.

--- src/main/java/io/github/nationalaudience/thetribunal/constant/LoginStaticValues.java
+++ src/main/java/io/github/nationalaudience/thetribunal/constant/LoginStaticValues.java
@@ -4,12 +4,21 @@ public class LoginStaticValues {
 
     public static final String END_POINT_LOGIN = "/login";
     public static final String END_POINT_POST_LOGIN = "/postLogin";
+    public static final String END_POINT_SIGNUP = "/signup";
+    public static final String END_POINT_POST_SIGNUP = "/postSignup";
 
     public static final String PARAMETER_USER = "user";
+    public static final String PARAMETER_NAME = "name";
+    public static final String PARAMETER_PASSWORD = "password";
+    public static final String PARAMETER_PASSWORD_CONFIRM = "passwordConfirm";
 
     public static final String ATTRIBUTE_USER_FOUND = "userFound";
     public static final String ATTRIBUTE_USER_NOT_FOUND = "userNotFound";
+    public static final String ATTRIBUTE_ERROR_MESSAGE = "errorMessage";
+    public static final String ATTRIBUTE_USERNAME = "username";
 
     public static final String TEMPLATE_LOGIN = "login/login_template";
+    public static final String TEMPLATE_SIGNUP = "login/signup_template";
+    public static final String TEMPLATE_SUCCESSFUL_SIGNUP = "login/signup_successful_template";
 
 }
