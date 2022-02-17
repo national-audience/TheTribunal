2988c548d567f680a86406b5f23141ba36c0b69a
Rafael Expósito Pérez <r.exposito.2018@gmail.com>
2022-02-17 11:55:17 +0100

    [FEAT] Add ranking sort by best games.

--- src/main/java/io/github/nationalaudience/thetribunal/controller/RankingController.java
+++ src/main/java/io/github/nationalaudience/thetribunal/controller/RankingController.java
@@ -1,16 +1,19 @@
 package io.github.nationalaudience.thetribunal.controller;
 
-import static io.github.nationalaudience.thetribunal.constant.RankingStaticValues.*;
-
-import static io.github.nationalaudience.thetribunal.constant.SearchStaticValues.ATTRIBUTE_HAS_USERS;
-
 import io.github.nationalaudience.thetribunal.repository.GameRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Collections;
+import java.util.ArrayList;
+
+import static io.github.nationalaudience.thetribunal.constant.RankingStaticValues.END_POINT_RANKING;
+import static io.github.nationalaudience.thetribunal.constant.RankingStaticValues.RANKING_TEMPLATE;
 
 @Controller
 public class RankingController {
@@ -22,12 +25,21 @@ public class RankingController {
     }
 
     @GetMapping(END_POINT_RANKING)
-    public String showRanking(Model model) {
+    public String showRanking(Model model, @RequestParam int page, @RequestParam int pageSize) {
 
-        var averageScores = gameRepository.getAllAverageScores();
+        var averageScores = gameRepository.getAllAverageScores(PageRequest.of(0, 2));
+        var gamesOrderByScore = gameRepository.getAllGamesByHighScore();
 
-        model.addAttribute("scores", averageScores);
+        var gameScores = new ArrayList<GameScore>();
+        for (int i = 0; i < averageScores.size(); i++) {
+            gameScores.add(new GameScore(gamesOrderByScore.get(i), averageScores.get(i)));
+        }
+
+        model.addAttribute("games", gameScores);
+
 
         return RANKING_TEMPLATE;
     }
+
+    private record GameScore (String name, float score) {}
 }
