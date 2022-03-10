00e93045a5741b2d12d0baa8ae08ee2fe6815b81
Gael Rial Costas <gael.rial.costas@gmail.com>
2022-03-10 12:32:22 +0100

    Starting login.

--- src/main/java/io/github/nationalaudience/thetribunal/constant/LoginStaticValues.java
+++ src/main/java/io/github/nationalaudience/thetribunal/constant/LoginStaticValues.java
@@ -3,7 +3,7 @@ package io.github.nationalaudience.thetribunal.constant;
 public class LoginStaticValues {
 
     public static final String END_POINT_LOGIN = "/login";
-    public static final String END_POINT_POST_LOGIN = "/postLogin";
+    public static final String END_POINT_ERROR_LOGIN = "/errorLogin";
     public static final String END_POINT_SIGNUP = "/signup";
     public static final String END_POINT_POST_SIGNUP = "/postSignup";
 
@@ -12,13 +12,14 @@ public class LoginStaticValues {
     public static final String PARAMETER_PASSWORD = "password";
     public static final String PARAMETER_PASSWORD_CONFIRM = "passwordConfirm";
 
-    public static final String ATTRIBUTE_USER_FOUND = "userFound";
-    public static final String ATTRIBUTE_USER_NOT_FOUND = "userNotFound";
+    public static final String ATTRIBUTE_INVALID_LOGIN = "invalidLogin";
     public static final String ATTRIBUTE_ERROR_MESSAGE = "errorMessage";
     public static final String ATTRIBUTE_USERNAME = "username";
 
     public static final String TEMPLATE_LOGIN = "login/login_template";
     public static final String TEMPLATE_SIGNUP = "login/signup_template";
     public static final String TEMPLATE_SUCCESSFUL_SIGNUP = "login/signup_successful_template";
+
+    public static final String CACHE_LOGGED_USER = "loggedUser";
 
 }
