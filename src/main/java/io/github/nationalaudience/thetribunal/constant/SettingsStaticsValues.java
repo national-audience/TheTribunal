c9fb94c66096c774e2fdacf20882e1c59e9de331
jvicog <javiervico8@gmail.com>
2022-04-03 20:14:02 +0200

    Settings

---
+++ src/main/java/io/github/nationalaudience/thetribunal/constant/SettingsStaticsValues.java
@@ -0,0 +1,12 @@
+package io.github.nationalaudience.thetribunal.constant;
+
+public class SettingsStaticsValues {
+
+    public static final String END_POINT_SETTINGS = "/settings";
+    public static final String END_POINT_SETTINGS_CONFIRM = "/settings/confirm";
+
+    public static final String ATTRIBUTE_USER_NAME = "user";
+    public static final String ATTRIBUTE_ERROR = "error";
+
+    public static final String SETTINGS_TEMPLATE = "settings/settings_template";
+}
