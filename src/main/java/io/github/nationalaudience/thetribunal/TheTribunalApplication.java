eab96ee5f3d70449498d9cedd21745cdf874346e
jvicog <javiervico8@gmail.com>
2022-05-02 19:04:46 +0200

    hazelcast

--- src/main/java/io/github/nationalaudience/thetribunal/TheTribunalApplication.java
+++ src/main/java/io/github/nationalaudience/thetribunal/TheTribunalApplication.java
@@ -1,13 +1,32 @@
 package io.github.nationalaudience.thetribunal;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+
+import java.util.Collections;
+import java.util.List;
 
 @SpringBootApplication
+@EnableHazelcastHttpSession
 public class TheTribunalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TheTribunalApplication.class, args);
     }
 
+    @Bean
+    public Config config(){
+        Config config = new Config();
+
+        JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+
+        joinConfig.getMulticastConfig().setEnabled(false);
+        joinConfig.getTcpIpConfig().setEnabled(true).setMembers(List.of("127.0.0.1"));
+
+        return config;
+    }
 }
