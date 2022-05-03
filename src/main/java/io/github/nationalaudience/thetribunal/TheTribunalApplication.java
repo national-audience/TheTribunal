b57b3fb93c9389e6c57c01033694d17ab2c5eb70
Gael Rial Costas <gael.rial.costas@gmail.com>
2022-05-03 13:18:38 +0200

    Cleanup main class.

--- src/main/java/io/github/nationalaudience/thetribunal/TheTribunalApplication.java
+++ src/main/java/io/github/nationalaudience/thetribunal/TheTribunalApplication.java
@@ -1,14 +1,10 @@
 package io.github.nationalaudience.thetribunal;
 
 import com.hazelcast.config.Config;
-import com.hazelcast.config.JoinConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
-
-import java.util.Collections;
-import java.util.List;
 
 @SpringBootApplication
 @EnableHazelcastHttpSession
@@ -17,9 +13,9 @@ public class TheTribunalApplication {
     public static void main(String[] args) {
         SpringApplication.run(TheTribunalApplication.class, args);
     }
-  
+
     @Bean
-    public Config config(){
+    public Config config() {
         var config = new Config();
         config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(true);
         return config;
