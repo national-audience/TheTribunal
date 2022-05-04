9494ae93a1289db80e41d15f9635b65477d822e1
jvicog <javiervico8@gmail.com>
2022-05-05 01:29:56 +0200

    cache

--- src/main/java/io/github/nationalaudience/thetribunal/repository/ReviewRepository.java
+++ src/main/java/io/github/nationalaudience/thetribunal/repository/ReviewRepository.java
@@ -3,11 +3,28 @@ package io.github.nationalaudience.thetribunal.repository;
 import io.github.nationalaudience.thetribunal.entity.Game;
 import io.github.nationalaudience.thetribunal.entity.Review;
 import io.github.nationalaudience.thetribunal.entity.User;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
         Optional<Review> findByUserAndGame(User user, Game game);
 
+        @CacheEvict(value="feed", allEntries = true)
+        @Override
+        <S extends Review> List<S> saveAll(Iterable<S> entities);
+
+        @CacheEvict(value="feed", allEntries = true)
+        @Override
+        <S extends Review> S saveAndFlush(S entity);
+
+        @CacheEvict(value="feed", allEntries = true)
+        @Override
+        <S extends Review> List<S> saveAllAndFlush(Iterable<S> entities);
+
+        @CacheEvict(value="feed", allEntries = true)
+        @Override
+        <S extends Review> S save(S entity);
 }
