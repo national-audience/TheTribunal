341888f7357ec0cf30e7366f2fd9a222352d4381
Rafael Expósito Pérez <r.exposito.2018@gmail.com>
2022-05-04 23:54:22 +0200

    Application login support for haproxy.

--- mysql/master/init.sql
+++ mysql/master/init.sql
@@ -10,14 +10,3 @@ CREATE USER IF NOT EXISTS 'slave_user'@'%' IDENTIFIED BY 'password';
 GRANT REPLICATION SLAVE ON *.* TO 'slave_user'@'%' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
-
-
-create table users
-(
-    id int auto_increment,
-    name varchar(255) null,
-    constraint users_pk
-        primary key (id)
-);
-
-INSERT INTO users VALUES (1, 'dipanjal'), (2, 'shohan');
